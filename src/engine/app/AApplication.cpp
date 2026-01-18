b887bd095010215cc5c0e41331de161fcd7d4a4e
ms-tristan <tristan.masse@epitech.eu>
2026-01-18 16:44:59 +0100

    chore: removed debug print

--- src/engine/app/AApplication.cpp
+++ src/engine/app/AApplication.cpp
@@ -188,11 +188,8 @@ void AApplication::run() {
   }
 
   while (_running) {
-    std::cout << "[D] Loop" << std::endl; 
     processMessages();
-    std::cout << "[D] PM Done" << std::endl;
     loop();
-    std::cout << "[D] Loop Done" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(10));
   }
 
